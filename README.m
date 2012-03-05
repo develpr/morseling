0997c7790305b9761bfe33616980b23b98bfc361
Vasileios Georgitzikis <georgitzik@ceid.upatras.gr>
2012-03-05 15:43:23 +0200

    Updated the readme

---
+++ README.m
@@ -0,0 +1,9 @@
+This is an MD5 library for the Arduino, based on scottmac's MD5 library, which you can find here:
+https://github.com/scottmac/arduino
+
+I created this because I was having a really hard time finding an easy-to-install and use libray for the Arduino,
+so I decided to make my own. There is an example on how to use it.
+
+### Installation
+Simply copy the MD5 folder, and paste in in the _libraries_ folder inside your Arduino sketch folder. If the
+libraries folder doesn't exist, create it.
