c023c914d5ddf4f0ba6acd452c2220a1e3b12154
develpr <kevin@develpr.com>
2013-06-17 23:34:54 -0700

    Starting to create actual file

--- src/Develpr/Morseling/Morseling.php
+++ src/Develpr/Morseling/Morseling.php
@@ -4,7 +4,12 @@ namespace Develpr\Morseling;
 
 class Morseling{
 	
+	protected $baseUri = "http://morsel.develpr.com";
+	protected $apiUri = "/api/v1/";
 	
+	protected $transmissionUri = "transmissions";
+	protected $messageUri = "messages";	
+
 	public function hello()
 	{
 		echo "HI";
