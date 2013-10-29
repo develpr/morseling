03088ec6a33d2c72af3c88426636cc840ef042ee
develpr <kevin@develpr.com>
2013-10-28 23:23:33 -0700

    Working on transmission/playback of messages

--- morseling.ino
+++ morseling.ino
@@ -1,15 +1,18 @@
-#include <MD5.h>
+#include <Bridge.h>
 
 /*
- *  Morse Sender
+ *  Morseling script
+ *
+ *  Requires a Yun!!!	
+ * 
+ *  @author Kevin Mitchell
  */
 
 // constants won't change. They're used here to 
 // set pin numbers:
 const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  10;      // the number of the LED pin
-const int secretKey = '12dsf9adf9w39sf032';
-const int username = 'develpr';
+const int ledPin =  13;      // the number of the LED pin
+long timer = 0; //a timer
 
 const unsigned int pauseToSendLength = 5000;
 
@@ -19,16 +22,17 @@ unsigned long lastChangeMilliseconds = 0;
 unsigned long now = 0;
 int currentButtonState = LOW;
 String output = "";
+String tsts = "";
+char value[1000];
 
 void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
-
   lastChangeMilliseconds = 0;
-
+  Bridge.begin();
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);      
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);     
+  timer = millis();
 }
 
 void loop(){
@@ -36,47 +40,81 @@ void loop(){
   buttonState = digitalRead(buttonPin);
 
   output = "";  
-  
-  if(buttonState == HIGH)
-  {
-    lastChangeMilliseconds = millis();
-    while(true)
-    {
-      digitalWrite(ledPin, HIGH);
-    
-      currentButtonState = digitalRead(buttonPin);
-      
-      if(buttonState != currentButtonState)
-      {
-        buttonState = currentButtonState;
-        output = output + "a" + buttonState + "b" + (String)(millis() - lastChangeMilliseconds);
-        lastChangeMilliseconds = millis();
-      }
-      
-      if((buttonState == LOW) && ((millis() - lastChangeMilliseconds) >= pauseToSendLength))
-      {        
-        break;
-      }
-    }    
-  }
-  
+  //todo: for some reason without a button actually hooked up the Yun seems to enter this loop and never leave
+//  
+//  if(buttonState == HIGH)
+//  {
+//    lastChangeMilliseconds = millis();
+//    while(true)
+//    {
+//      //digitalWrite(ledPin, HIGH);
+//    
+//      currentButtonState = digitalRead(buttonPin);
+//      
+//      if(buttonState != currentButtonState)
+//      {
+//        buttonState = currentButtonState;
+//        output = output + "a" + buttonState + "b" + (String)(millis() - lastChangeMilliseconds);
+//        lastChangeMilliseconds = millis();
+//      }
+//      
+//      if((buttonState == LOW) && ((millis() - lastChangeMilliseconds) >= pauseToSendLength))
+//      {        
+//        break;
+//      }
+//    }    
+//  }
+//  
   digitalWrite(ledPin, LOW);
-  
+    
   if(output != "")
   {
-    //Here is where we send output
-    Serial.println(output);
-    Serial.println("\n\nHash: ");   
+
+    //Get the first message buffer
+    Bridge.get("message1", value, 1000);
+    String tsts = String(value);
+
+    //If the first message buffer is free, which it probably will be
+    //then we'll go ahead and write our message to it
+    if(tsts.length() == 0)
+      Bridge.put("message1", output);
+    else
+    {
+
+      Bridge.get("message2", value, 1000);
+      tsts = String(value);
+      if(tsts.length() == 0)
+        Bridge.put("message2", output);
+      else
+      {
+	Bridge.put("message3", output);
+      }
+
+    }
+  }
+  
+  
+  /* Every 200ms: */
+
+  if (millis() - timer > 500) {
+    timer = millis();
+    Bridge.get("play1", value, 1000);
+    String tsts = String(value);
     
-    String saltedString = output + secretKey;
-    char* testing;
-    saltedString.toCharArray(testing, saltedString.length());
+    if(tsts.length() > 0)
+    {
+      //todo: play back the message!
+      digitalWrite(ledPin, HIGH);
+      delay(2000);
+      digitalWrite(ledPin, LOW);
+      delay(2000);
+      digitalWrite(ledPin, HIGH);
+      delay(2000);
+      digitalWrite(ledPin, LOW);
+      delay(2000);
+      Bridge.put("play1", "");
+    }    
     
-    unsigned char* test = MD5::make_hash(testing);
-    char *md5str = MD5::make_digest(test, 16);
-    Serial.println(md5str);
-    
-    Serial.println("\n\n\n\n");   
   }
   
   //Here we should check to see if any new messages are here, every few seconds
