51c98d39e582bc86d7be22f49d2138e1c313ac91
develpr <kevin@develpr.com>
2013-10-30 22:50:37 -0700

    Update arduino_send.py

--- morseling-python/Python/arduino_send.py
+++ morseling-python/Python/arduino_send.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python
+
 import json,morseling,sys
+
+from bridge.bridgeclient import BridgeClient
+
+bridge_client = BridgeClient()
 
 req = morseling.morseling()
 
@@ -7,7 +13,23 @@ req = morseling.morseling()
 #           0b200 (0 represents a "pause" between presses, 200 represents length of pause)
 #           a is the seperator between press/pause and length key pairs
 
-##SOS
-input_timings = str(sys.argv[1])
-req.send_message_with_timings(input_timings)
 
+message1 = bridge_client.get("message1")
+message2 = bridge_client.get("message2")
+message3 = bridge_client.get("message3")
+
+if(message1):
+        #send message via morsel api
+        req.send_message_with_timings(message1)
+      	bridge_client.put('message1', '')
+                        
+if(message2):
+	#send message via morsel api
+	req.send_message_with_timings(message2)
+       	bridge_client.put('message2', '')
+                                                
+                                                
+if(message3):
+	#send this message too
+	req.send_message_with_timings(message3)
+        bridge_client.put('message3', '')
