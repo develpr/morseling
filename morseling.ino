46ac5eb0341494aff0b20df34d9e924371565104
develpr <kevin@develpr.com>
2013-11-04 07:39:26 -0800

    Include capacitive touch sensor for changing speed

--- morseling.ino
+++ morseling.ino
@@ -1,4 +1,5 @@
 #include <Bridge.h>
+#include <CapacitiveSensor.h>
 
 /*
  *  Morseling script
@@ -13,10 +14,13 @@
 const int buttonPin = 2;     // the number of the pushbutton pin
 const int ledPin =  10;      // the number of the LED pin
 long timer = 0; //a timer
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,3);
+CapacitiveSensor   cs_4_5 = CapacitiveSensor(4,5);
 
 const unsigned int pauseToSendLength = 5000;
 
 // variables will change:
+int playbackMultiplier = 5;
 int buttonState = 0;         // variable for reading the pushbutton status
 unsigned long lastChangeMilliseconds = 0;
 unsigned long now = 0;
@@ -28,10 +32,13 @@ String buffer = "";
 char action = ' ';
 char previous = ' ';
 char current = ' ';
+long replayButton = 0;
+long multiplyButton = 0;
 
 void setup() {
   lastChangeMilliseconds = 0;
   Bridge.begin();
+
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);      
   // initialize the pushbutton pin as an input:
@@ -42,6 +49,36 @@ void setup() {
 void loop(){
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
+  replayButton =  cs_4_2.capacitiveSensor(30);
+  multiplyButton =  cs_4_5.capacitiveSensor(30);
+  
+  //Should we change the message playback multiplier?
+  if(multiplyButton > 100)
+  {
+    playbackMultiplier = playbackMultiplier * 1.2;
+    if(playbackMultiplier > 26)
+      playbackMultiplier = 5;
+      
+    if(playbackMultiplier == 5)
+      beepXTimes(1);
+    else if(playbackMultiplier >= 7 && playbackMultiplier <= 8)
+      beepXTimes(2);
+    else if(playbackMultiplier >= 11 && playbackMultiplier <= 12)
+      beepXTimes(3);
+    else if(playbackMultiplier >= 16 && playbackMultiplier <= 17)
+      beepXTimes(4);
+    else if(playbackMultiplier >= 25 && playbackMultiplier <= 26)
+      beepXTimes(5);      
+  }
+
+  //Check if replay button has been pressed
+  if(replayButton > 100)
+  {
+    Bridge.get("replay", value, 1000);
+    String toPlay = String(value);
+    play(toPlay, playbackMultiplier);  
+  }
+  
 
   output = "";  
   //todo: for some reason without a button actually hooked up the Yun seems to enter this loop and never leave
@@ -103,53 +140,70 @@ void loop(){
   //todo: TEST THIS!!!!!
   //Here we should check to see if any new messages are here, every few seconds
   //http://jsfiddle.net/8JTLU/4/
-  if (millis() - timer > 5/00) {
+  if (millis() - timer > 500) {
     timer = millis();
     Bridge.get("play1", value, 1000);
     String toPlay = String(value);
     
     if(toPlay.length() > 0)
     {
-      buffer = "";
-      action = ' ';
-      current = ' ';
-      previous = ' ';
-      for(int i = 1; i < toPlay.length(); i++)
-      {
-        current = toPlay.charAt(i);
-        previous = toPlay.charAt(i-1);
-        if(current == 'a')
-        {
-          char bufferTime[buffer.length()];
-          buffer.toCharArray(bufferTime, buffer.length());   
-          int toPlay = atoi(bufferTime);       
-          if(action == '0'){
-
-            playSound(toPlay);
-          }            
-          else
-            delay(toPlay);
-          buffer = "";
-          action = ' ';
-        }
-        
-        if(previous == 'a')
-          action = current;
-          
-        else if(current == 'b')
-          buffer = "";
-         
-         else
-           buffer += String(current);
-          
-      }
+      play(toPlay, playbackMultiplier);      
       Bridge.put("play1", "");
     }    
     
   }
   
+}
 
+void beepXTimes(int times)
+{
+  for(int i = 0; i < times; i++)
+  {
+    digitalWrite(ledPin, HIGH);
+    delay(400);
+    digitalWrite(ledPin, LOW);
+    delay(400);
+  } 
+}
 
+void play(String toPlay, int multiplier)
+{
+  buffer = "";
+  action = ' ';
+  current = ' ';
+  previous = ' ';
+  for(int i = 1; i < toPlay.length(); i++)
+  {
+    current = toPlay.charAt(i);
+    previous = toPlay.charAt(i-1);
+    if(current == 'a')
+    {
+      char bufferTime[buffer.length()];
+      buffer.toCharArray(bufferTime, buffer.length());   
+      int toPlay = atoi(bufferTime);       
+      if(action == '0'){
+
+        playSound(toPlay * multiplier);
+      }            
+      else
+      {
+        digitalWrite(ledPin, LOW);
+        delay(toPlay * multiplier);
+      }
+      buffer = "";
+      action = ' ';
+    }
+    
+    if(previous == 'a')
+      action = current;
+      
+    else if(current == 'b')
+      buffer = "";
+     
+     else
+       buffer += String(current);
+      
+  }
 }
 
 /*
@@ -158,4 +212,7 @@ void loop(){
  */
 void playSound(int playTime){
   //play sound!!
+  digitalWrite(ledPin, HIGH);
+  delay(playTime);
+  digitalWrite(ledPin, LOW);
 }
