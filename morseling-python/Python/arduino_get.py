28f21ca1ac3cdae402fc0590482ec2855d26e977
develpr <kevin@develpr.com>
2013-11-23 21:10:13 -0800

    Fixed spacing (I hope)

--- morseling-python/Python/arduino_get.py
+++ morseling-python/Python/arduino_get.py
@@ -14,20 +14,20 @@ def get_messages():
 	transmission = req.get_transmission()
 
 	if(transmission != False):
-	    # Get the raw message
-	    raw_message = transmission[0]["message"]["raw"]
-	    
-	    # todo: use Bridge to send this
-	    
-	    req.receive_transmission(transmission[0]["id"])
-        
+        # Get the raw message
+        raw_message = transmission[0]["message"]["raw"]
+
+        # todo: use Bridge to send this
+
+        req.receive_transmission(transmission[0]["id"])
+
         # Because of an apparent limit in Arduino's Bridge library when using set/get, we're going to
         # split the message up into 250 character parts
         playIndex = 0
-        
-        while(len(raw_message) > 0):            
-    	    bridge_setter.put("play1" + `playIndex`, raw_message[0:BRIDGE_CHARACTER_LIMIT])
-    	    bridge_setter.put("replay" + `playIndex`, raw_message[0:BRIDGE_CHARACTER_LIMIT])
+
+        while(raw_message a len(raw_message) > 0):            
+            bridge_setter.put("play1" + `playIndex`, raw_message[0:BRIDGE_CHARACTER_LIMIT])
+            bridge_setter.put("replay" + `playIndex`, raw_message[0:BRIDGE_CHARACTER_LIMIT])
             raw_message = raw_message[BRIDGE_CHARACTER_LIMIT:]
     	    
 
