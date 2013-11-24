bac0cc2a881d536af6c6fdf5b17b22579c29950d
develpr <kevin@develpr.com>
2013-11-23 17:40:53 -0800

    Update arduino_send.py

--- morseling-python/Python/arduino_send.py
+++ morseling-python/Python/arduino_send.py
@@ -13,27 +13,27 @@ req = morseling.morseling()
 #           0b200 (0 represents a "pause" between presses, 200 represents length of pause)
 #           a is the seperator between press/pause and length key pairs
 
+def send_messages():
+	message1 = bridge_client.get("message1")
+	message2 = bridge_client.get("message2")
+	message3 = bridge_client.get("message3")
 
-message1 = bridge_client.get("message1")
-message2 = bridge_client.get("message2")
-message3 = bridge_client.get("message3")
-
-if(message1):
-        #send message via morsel api
-	req.send_message_with_timings(message1)
-	bridge_client.put('message1', '')
+	if(message1):
+	        #send message via morsel api
+	        req.send_message_with_timings(message1)
+	      	bridge_client.put('message1', '')
                         
-if(message2):
-	#send message via morsel api
-	req.send_message_with_timings(message2)
-	bridge_client.put('message2', '')
+	if(message2):
+		#send message via morsel api
+		req.send_message_with_timings(message2)
+	       	bridge_client.put('message2', '')
                                                 
                                                 
-if(message3):
-	#send this message too
-	req.send_message_with_timings(message3)
-	bridge_client.put('message3', '')
-
+	if(message3):
+		#send this message too
+		req.send_message_with_timings(message3)
+	        bridge_client.put('message3', '')
+	        
 send_messages()
 time.sleep(10)
 send_messages()
